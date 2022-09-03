770ff8643cc774c6684dc5de5c2ebc77c275cce1
dataXcode <63473696+dataXcode@users.noreply.github.com>
2022-09-03 19:07:25 +0300

    Update exc_01_03.py

--- exercises/exc_01_03.py
+++ exercises/exc_01_03.py
@@ -1,7 +1,12 @@
-def func(a,b, g=5):
-    x = _*_
-    y = _+_
-    z = _______
-    return y, z, g
-
-___________
+import random as randomlib
+random = randomlib.randint(1, 100)
+print("Guess a number between 1 and 100")
+while True:
+    guess = int(input())
+    if guess < random:
+        print("Too low")
+    elif guess > random:
+        print("Too high")
+    else:
+        print("That's right!")
+        break
